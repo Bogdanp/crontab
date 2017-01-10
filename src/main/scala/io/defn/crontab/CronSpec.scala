0c941ea72da3f870e245c1937735bf547d92813f
Bogdan Popa <popa.bogdanp@gmail.com>
2017-01-10 11:01:11 +0200

    add Ordering to Month and Weekday

--- src/main/scala/io/defn/crontab/CronSpec.scala
+++ src/main/scala/io/defn/crontab/CronSpec.scala
@@ -1,10 +1,31 @@
 package io.defn.crontab
 
-case class Minute(val value: Int) extends AnyVal
-case class Hour(val value: Int) extends AnyVal
-case class Day(val value: Int) extends AnyVal
+final case class Minute(val value: Int) extends AnyVal
+final case class Hour(val value: Int) extends AnyVal
+final case class Day(val value: Int) extends AnyVal
 
-sealed trait Month
+/*** Sum type representing the months of the year. */
+sealed trait Month extends Ordering[Month] {
+  def compare(a: Month, b: Month): Int =
+    a.ord compare b.ord
+
+  def ord: Int =
+    this match {
+      case Jan => 1
+      case Feb => 2
+      case Mar => 3
+      case Apr => 4
+      case May => 5
+      case Jun => 6
+      case Jul => 7
+      case Aug => 8
+      case Sep => 9
+      case Oct => 10
+      case Nov => 11
+      case Dec => 12
+    }
+}
+
 final case object Jan extends Month
 final case object Feb extends Month
 final case object Mar extends Month
@@ -18,7 +39,23 @@ final case object Oct extends Month
 final case object Nov extends Month
 final case object Dec extends Month
 
-sealed trait Weekday
+/*** Sum type representing the days of the week. */
+sealed trait Weekday extends Ordering[Weekday] {
+  def compare(a: Weekday, b: Weekday): Int =
+    a.ord compare b.ord
+
+  def ord: Int =
+    this match {
+      case Mon => 1
+      case Tue => 2
+      case Wed => 3
+      case Thu => 4
+      case Fri => 5
+      case Sat => 6
+      case Sun => 7
+    }
+}
+
 final case object Mon extends Weekday
 final case object Tue extends Weekday
 final case object Wed extends Weekday
